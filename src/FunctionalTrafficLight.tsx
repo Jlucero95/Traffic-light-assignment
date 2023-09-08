036d769177b7fb4514e421468388f3c492fa72f6
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-09-07 20:27:07 -0500

    Finished Logic for FunctionalTrafficLight
    Talk to jon about refactor.

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+	const [redLightOn, setRedLightOn] = useState(true);
+	const [yellowLightOn, setYellowLightOn] = useState(false);
+	const [greenLightOn, setGreenLightOn] = useState(false);
+
+	const toggleState = () => {
+		if (redLightOn) {
+			setRedLightOn(false);
+			setYellowLightOn(true);
+		} else if (yellowLightOn) {
+			setYellowLightOn(false);
+			setGreenLightOn(true);
+		} else if (greenLightOn) {
+			setGreenLightOn(false);
+			setRedLightOn(true);
+		}
+	};
+
 	return (
 		<div className="traffic-light-box">
 			<h2>Functional Traffic Light</h2>
 			<div className="traffic-light">
-				{/* Background color can be black | yellow | red | green */}
-				<div className="circle black"></div>
-				<div className="circle yellow"></div>
-				<div className="circle green"></div>
+				<div className={`circle ${redLightOn ? "red" : "black"}`}></div>
+				<div className={`circle ${yellowLightOn ? "yellow" : "black"}`}></div>
+				<div className={`circle ${greenLightOn ? "green" : "black"}`}></div>
 			</div>
-			<button className="next-state-button">Next State</button>
+			<button
+				className="next-state-button"
+				onClick={toggleState}
+			>
+				Next State
+			</button>
 		</div>
 	);
 };
